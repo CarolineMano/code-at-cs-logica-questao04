94183e0c9ddf5082a6e3c61fb01926a3e0d2e6a8
CarolineMano <carolinemano@protonmail.ch>
2022-12-18 12:40:12 -0300

    Questão 04

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Questao04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(funcaoRetornaQuantidadePOV(0.1M, 90)); //10
+            Console.WriteLine(funcaoRetornaQuantidadePOV(0.1M, 100)); //11
+            Console.WriteLine(funcaoRetornaQuantidadePOV(0.2M, 70)); //17
+        }
+
+        static decimal funcaoRetornaQuantidadePOV(decimal porcentagem, int totalNegociado)
+        {
+            var totalComEstrategia = (int)(totalNegociado / (1 - porcentagem));
+
+            return (int)(totalComEstrategia * porcentagem);
+        }
+    }
+}
